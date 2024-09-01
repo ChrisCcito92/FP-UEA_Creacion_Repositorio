6ae753ab737e129f4529406b1cf9065d71d47103
Chris <cx.calvag@uea.edu.ec>
2024-09-01 16:44:22 -0500

    Opreaciones con arreglos de la semana 11

--- Sem 11 - Opreaciones con arreglos/Ordenación de Arreglo Multidimensional.py
+++ Sem 11 - Opreaciones con arreglos/Ordenación de Arreglo Multidimensional.py
@@ -4,3 +4,16 @@ matriz = [
     [3,7,2],
     [0,5,4]
 ]
+# Usamos la función usando buble sort
+def buble_sort(fila):
+    num = len(fila)
+    for i in range(num):
+        for j in range(num-1, i, -1):
+            if fila[j] > fila[j-1]:
+                fila[j], fila[j-1] = fila[j-1], fila[j]
+                print(fila)
+
+# Imprimimos las matrices
+print("Matriz original ", matriz)
+buble_sort(matriz[1])
+print("Matriz ordenada de mayor a menor ", matriz)
